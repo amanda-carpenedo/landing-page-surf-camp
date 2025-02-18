cb87dd65928f6f874190d3702e7ec5735cad9307
Amanda Carpenedo <amanda_carpenedo@hotmail.com>
2025-02-18 12:47:09 +0100

    new commit

--- reveal.js
+++ reveal.js
@@ -5,3 +5,16 @@ revelar.reveal('.efeito-txt-hero',
         duration: 3000,
         distance: '30px'
     })
+
+    const menuDiv = document.getElementById ('menu-mobile')
+const btnAnimar = document.getElementById ('btn-menu-mobile')
+const body = document.body;
+
+menuDiv.addEventListener('click', animar)
+
+function animar() {
+    menuDiv.classList.toggle('abrir')
+    btnAnimar.classList.toggle('ativar')
+
+    body.classList.toggle('no-overflow')
+}
